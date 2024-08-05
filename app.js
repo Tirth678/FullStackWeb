1f9e97e3bf7722975ce78695d757c9eba4f95cc1
Tirth S Pandya <teamfznltd@gmail.com>
2024-08-05 14:32:15 +0530

    commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// function factorial(x) {
+//   if (x == 0 || x === 1) {
+//     return 1;
+//   } else {
+//     return x * factorial(x - 1);
+//   }
+// }
+// let num = 4;
+// console.log(factorial(num));
+// function add(x) {
+//   if (x == 0) {
+//     return 0;
+//   } else {
+//     return x + add(x - 1);
+//   }
+// }
+// let num = 5000;
+// console.log(add(num));
+console.log("hello mac!\n");
